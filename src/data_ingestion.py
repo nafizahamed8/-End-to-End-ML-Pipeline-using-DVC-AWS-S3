813d82200f266eb03c8e8a9e9a7fa9a0b139a1f1
nafizahamed8 <139810043+nafizahamed8@users.noreply.github.com.>
2025-11-06 03:02:29 +0600

    params configured for the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 import logging
+import yaml
 
 #ensure the log directory exists
 log_dir="logs"
@@ -26,6 +27,24 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+
+
+def load_params(param_path:str):
+    """load parameter from yaml file"""
+    try:
+        with open(param_path,'r') as file:
+            params=yaml.safe_load(file)
+        logger.debug("parameters retrieve from %s",param_path)
+        return params
+    except FileNotFoundError:
+        logger.error("file not found: %s",param_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error("YAML error: %s",e)
+        raise
+    except Exception as e:
+        logger.error("unexpected error: %s",e)
+        raise
 
 def load_data(data_url:str):
     """load data from csv file"""
@@ -70,7 +89,9 @@ def save_data(train_data:pd.DataFrame,test_data:pd.DataFrame,data_path:str):
 
 def main():
     try:
-        test_size=0.2
+        params=load_params(param_path='params.yaml')
+        test_size=params['data_ingestion']['test_size']
+        #test_size=0.2
         data_path="https://raw.githubusercontent.com/nafizahamed8/Basic-Machine-Learning-Projects/refs/heads/main/spam.csv"
         df=load_data(data_url=data_path)
         final_df=preprocess_data(df)
